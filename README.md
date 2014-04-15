fadaeda4f446557ae840fa2331219fea671cc951
Christopher Nager <cnager@gmail.com>
2014-04-15 13:36:09 -0400

    Add index and script

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-tiny-google-fonts
-=================
+# tiny google fonts
 
 Automatically reduce your Google Fonts file size by only building the font with the characters you actually use.
---
+++ tiny-google-fonts.js
@@ -0,0 +1,20 @@
+var tinyGoogleFontsScript = document.querySelector('script[src*="tiny-google-fonts"]'),
+    googleFontClass = tinyGoogleFontsScript.getAttribute('data-font-class'), // Optional. Default is entire body text
+    usedCharacters = document.body.innerText.replace(/(.)(?=.*\1)/g, ""),
+    googleFontFamily = tinyGoogleFontsScript.getAttribute('data-font-family'),
+    googleFontWeight = ':' + tinyGoogleFontsScript.getAttribute('data-font-weight') // Should be optional
+    googleFontStyle = tinyGoogleFontsScript.getAttribute('data-font-style') // Should be optional
+    googleFontsLink = '<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=' + googleFontFamily + googleFontWeight + '&text=' + usedCharacters + '%20">';
+
+console.log(tinyGoogleFontsScript);
+console.log(googleFontClass);
+console.log(usedCharacters);
+console.log(googleFontFamily);
+console.log(googleFontWeight);
+console.log(googleFontStyle);
+console.log(googleFontsLink);
+
+//googleFontFamily.innerText.replace(/\s/g, '+');
+//console.log(googleFontFamily);
+
+document.head.innerHTML += googleFontsLink;
