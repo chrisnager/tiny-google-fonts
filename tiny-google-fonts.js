b57636725a0a3156fbad411934d173a840b0972c
Christopher Nager <cnager@gmail.com>
2014-04-16 15:30:02 -0400

    Update font and add style

--- tiny-google-fonts.js
+++ tiny-google-fonts.js
@@ -1,17 +1,15 @@
 var tinyGoogleFontsScript = document.querySelector('script[src*="tiny-google-fonts"]'),
-    googleFontClass = tinyGoogleFontsScript.getAttribute('data-font-class'), // Optional. Default is entire body text
-    usedCharacters = '',
+    usedCharacters = usedCharacters = document.body.innerText.replace(/(.)(?=.*\1)/g, ""),
+    googleFontClass = tinyGoogleFontsScript.getAttribute('data-font-class'), // Should be optional
     findCharacters = function() {
         if (googleFontClass) {
             usedCharacters = googleFontClass.innerText.replace(/(.)(?=.*\1)/g, "");
-        } else {
-            usedCharacters = document.body.innerText.replace(/(.)(?=.*\1)/g, "");
         }
     },
     googleFontFamily = tinyGoogleFontsScript.getAttribute('data-font-family'),
-    googleFontWeight = ':' + tinyGoogleFontsScript.getAttribute('data-font-weight') // Should be optional
-    googleFontStyle = tinyGoogleFontsScript.getAttribute('data-font-style') // Should be optional
-    googleFontsLink = '<link rel="stylesheet" href="//fonts.googleapis.com/css?family=' + googleFontFamily + googleFontWeight + '&text=' + usedCharacters + '%20">';
+    googleFontWeight = ':' + tinyGoogleFontsScript.getAttribute('data-font-weight'), // Should be optional
+    googleFontStyle = tinyGoogleFontsScript.getAttribute('data-font-style'), // Should be optional
+    googleFontsLink = '<link rel="stylesheet" href="//fonts.googleapis.com/css?family=' + googleFontFamily + googleFontWeight + googleFontStyle + '&text=' + usedCharacters + '%20">';
 
 console.log(tinyGoogleFontsScript);
 console.log(googleFontClass);
