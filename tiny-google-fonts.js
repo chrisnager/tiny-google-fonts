3ec56902745cd8baa1815f9e9f567110d24fd9c8
Christopher Nager <cnager@gmail.com>
2014-04-28 16:58:55 -0400

    Clean up js, simplify by removing separate font weights and styles

--- tiny-google-fonts.js
+++ tiny-google-fonts.js
@@ -1,25 +1,15 @@
-var tinyGoogleFontsScript = document.querySelector('script[src*="tiny-google-fonts"]'),
-    usedCharacters = usedCharacters = document.body.innerText.replace(/(.)(?=.*\1)/g, ""),
-    googleFontClass = tinyGoogleFontsScript.getAttribute('data-font-class'), // Should be optional
-    findCharacters = function() {
-        if (googleFontClass) {
-            usedCharacters = googleFontClass.innerText.replace(/(.)(?=.*\1)/g, "");
-        }
-    },
-    googleFontFamily = tinyGoogleFontsScript.getAttribute('data-font-family'),
-    googleFontWeight = ':' + tinyGoogleFontsScript.getAttribute('data-font-weight'), // Should be optional
-    googleFontStyle = tinyGoogleFontsScript.getAttribute('data-font-style'), // Should be optional
-    googleFontsLink = '<link rel="stylesheet" href="//fonts.googleapis.com/css?family=' + googleFontFamily + googleFontWeight + googleFontStyle + '&text=' + usedCharacters + '%20">';
+var tinyGoogleFontsScript = document.querySelector('script[src*="tiny-google-fonts"]');
+var googleFontClass = tinyGoogleFontsScript.getAttribute('data-font-class');
+var googleFonts= tinyGoogleFontsScript.getAttribute('data-fonts');
+var usedCharacters = document.body.innerText.replace(/\n| |(.)(?=.*\1)/g, '');
+var googleFontsLink = '<link rel="stylesheet" href="//fonts.googleapis.com/css?family=' + googleFonts + '&text=' + usedCharacters + '%20">';
 
-console.log(tinyGoogleFontsScript);
-console.log(googleFontClass);
-console.log(usedCharacters);
-console.log(googleFontFamily);
-console.log(googleFontWeight);
-console.log(googleFontStyle);
-console.log(googleFontsLink);
+findUsedCharacters = function() {
+    if (googleFontClass) {
+        usedCharacters = document.querySelector('.' + googleFontClass).innerText.replace(/(.)(?=.*\1)|\n| /g, '');
+    }
+}
 
-//googleFontFamily.innerText.replace(/\s/g, '+');
-//console.log(googleFontFamily);
-
+findUsedCharacters();
 document.head.innerHTML += googleFontsLink;
+document.body.parentNode.classList.add('tiny-google-fonts');
